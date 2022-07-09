8ab79ac4542b673e02aba618e884619ccce7aaab
AbhishekGowda28 <abhi.gowda28@gmail.com>
2022-07-10 00:15:37 +0530

    chore: add script to run project

---
+++ index.ts
@@ -0,0 +1,36 @@
+import { exec } from 'child_process';
+import { readdirSync } from 'fs';
+import { join } from 'path';
+
+const sourceDirectory = 'src';
+
+const fullPath = join(__dirname, sourceDirectory);
+
+try {
+    executeFiles(fullPath);
+
+    function executeFiles(directoryPath: string) {
+
+        readdirSync(directoryPath, { withFileTypes: true }).forEach(file => {
+            const filePath = join(directoryPath, file.name);
+            if (file.isDirectory()) {
+                executeFiles(filePath);
+            } else {
+                const command = `ts-node ${filePath}`;
+                exec(command, (error, stdout, stderr) => {
+                    if (error) {
+                        console.error(error);
+                    }
+                    if (stderr) {
+                        console.error(stderr);
+                    }
+                    console.log(`File ${filePath}`);
+                    console.log(stdout);
+                });
+            }
+        });
+    }
+
+} catch (error) {
+    console.error('Error in executeFiles', { error });
+}
