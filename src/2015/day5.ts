3624961597c5374b98052b3600012f10c6d99066
AbhishekGowda28 <abhi.gowda28@gmail.com>
2022-07-15 21:50:09 +0530

    Partial completation of 2015 day5

--- src/2015/day5.ts
+++ src/2015/day5.ts
@@ -1,58 +1,62 @@
 /**
  * Problem Statement: https://adventofcode.com/2015/day/5
- * 
+ *
  * Rules
  * -----
  * 1. Contains atleast 3 any vowels
  * 2. Contains repeated characters
  * 3. Doesn't contain [ab, cd, pq, xy]
- * 
+ *
  */
 
-import { getDataFromFile } from './getDataFromFile';
+import { getDataFromFile } from "./getDataFromFile";
 
-const fileName = `${__filename.split('.')[0]}.txt`;
+const fileName = `${__filename.split(".")[0]}.txt`;
 
 let input = getDataFromFile(fileName);
 
 function numberOfVowels(str: string) {
-    const input = str.split('');
-    const vowelArray = ["a", "e", "i", "o", "u"];
-    return input.reduce((count, currentString) => {
-        vowelArray.includes(currentString) && count++;
-        return count;
-    }, 0);
+  const input = str.toLocaleLowerCase().split("");
+  const vowelArray = ["a", "e", "i", "o", "u"];
+  return input.reduce((count, currentString) => {
+    vowelArray.includes(currentString) && count++;
+    return count;
+  }, 0);
 }
 
 function containRepetativeCharacters(str: string) {
-    return str.split('').filter((ele, index, arr) => {
-        const nearestIndex = arr.indexOf(ele);
-        return nearestIndex !== index && nearestIndex === index - 1
-    }).length;
+  return str.split("").filter((ele, index, arr) => {
+    const nearestIndex = arr.indexOf(ele);
+    return nearestIndex !== index && nearestIndex === index - 1;
+  }).length;
 }
 
 function containsRestristedCharacters(str: string) {
-    const restrictedArray = ["ab", "cd", "pq", "xy"];
-    return restrictedArray.filter(key => str.includes(key)).length;
+  const restrictedArray = ["ab", "cd", "pq", "xy"];
+  return restrictedArray.filter((key) => str.includes(key)).length;
 }
 
 function isStringNice(str: string) {
-    const vowels = numberOfVowels(str);
-    const repetativeStr = containRepetativeCharacters(str);
-    const hasRestristiveWord = containsRestristedCharacters(str);
-    return (vowels >= 3) && (repetativeStr >= 1) && (hasRestristiveWord === 0);
+  const vowels = numberOfVowels(str);
+  const repetativeStr = containRepetativeCharacters(str);
+  const hasRestristiveWord = containsRestristedCharacters(str);
+  // console.log({ vowels, repetativeStr, hasRestristiveWord });
+  return vowels > 2 && repetativeStr >= 1 && hasRestristiveWord === 0;
 }
 
 function numberOfNiceStrings(data: string) {
-    const str = data.split('\n');
-    let counter = 0;
-    str.forEach((ele) => {
-        if (isStringNice(ele)) {
-            counter++;
-        }
-    });
-    return counter;
+  const str = data.split("\r\n");
+  let counter = 0;
+  str.forEach((ele) => {
+    if (isStringNice(ele)) {
+      console.log(ele);
+      counter++;
+    }
+  });
+  return counter;
 }
+
+// input = 'ugknbfddgicrmopn';
 
 const output = numberOfNiceStrings(input);
 console.log({ output });
