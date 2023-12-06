2724ea4746745640342ad7571f24ab6d96432e34
AbhishekGowda28 <abhi.gowda28@gmail.com>
2023-12-07 01:06:48 +0530

    a small update

--- index.ts
+++ index.ts
@@ -1,41 +1,38 @@
-import { exec } from 'child_process';
-import { readdirSync } from 'fs';
-import { join } from 'path';
+import { exec } from "child_process";
+import { readdirSync } from "fs";
+import { join } from "path";
 
-const sourceDirectory = 'src';
+const sourceDirectory = "src";
 
 const fullPath = join(__dirname, sourceDirectory);
+executeFiles(fullPath);
 
-try {
-    executeFiles(fullPath);
-
-    function executeFiles(directoryPath: string) {
-
-        readdirSync(directoryPath, { withFileTypes: true }).forEach(file => {
-            const filePath = join(directoryPath, file.name);
-            if (file.isDirectory()) {
-                executeFiles(filePath);
-            } else {
-                /**
-                 * Execute only ts files
-                 */
-                if (file.name.split('.')[1] === 'ts') {
-                    const command = `ts-node ${filePath}`;
-                    exec(command, (error, stdout, stderr) => {
-                        if (error) {
-                            console.error(error);
-                        }
-                        if (stderr) {
-                            console.error(stderr);
-                        }
-                        console.log(`File ${filePath}`);
-                        console.log(stdout);
-                    });
-                }
+function executeFiles(directoryPath: string) {
+  try {
+    readdirSync(directoryPath, { withFileTypes: true }).forEach((file) => {
+      const filePath = join(directoryPath, file.name);
+      if (file.isDirectory()) {
+        executeFiles(filePath);
+      } else {
+        /**
+         * Execute only ts files
+         */
+        if (file.name.split(".")[1] === "ts") {
+          const command = `ts-node ${filePath}`;
+          exec(command, (error, stdout, stderr) => {
+            if (error) {
+              console.error(error);
             }
-        });
-    }
-
-} catch (error) {
-    console.error('Error in executeFiles', { error });
+            if (stderr) {
+              console.error(stderr);
+            }
+            console.log(`File ${filePath}`);
+            console.log(stdout);
+          });
+        }
+      }
+    });
+  } catch (error) {
+    console.error("Error in executeFiles", { error });
+  }
 }
